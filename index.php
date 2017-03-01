4bf03e0880c2a9e77aea6be17e4e213712e309d3
carmac <kpc@live.ie>
2017-03-01 13:22:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-echo "eh... built it!!!!!!!!!!!!!!!!!";
-echo "composer test";
-echo "final composer test";
-
-require 'vendor/autoload.php';
-require 'Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
-
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-$app->run();
+echo "works";
 ?>
