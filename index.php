7f08072cf36044078a59a28ff0295e06d2f7030e
carmac <kpc@live.ie>
2017-03-06 11:09:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "working<br/>Test before perms mod<br/>";
 echo "Test 1:EBS Full Access<br/>";
 echo "Test 2: Custom EBS Full Access<br/>";
+echo "Test 3: Custom EBS Web Server Env Access<br/>";
 ?>
