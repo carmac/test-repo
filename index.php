f898cbb395be139aa85be1ea64d75223f1171356
carmac <kpc@live.ie>
2017-03-06 12:19:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ echo "Test 1:EBS Full Access<br/>";
 echo "Test 2: Custom EBS Full Access<br/>";
 echo "Test 3: Custom EBS Web Server Env Access<br/>";
 echo "Test 4: Custom EBS Web Server Env Access--limited<br/>";
+echo "Test 5: Final Test<br/>";
 ?>
